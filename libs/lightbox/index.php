0d90daa38af7c49eba0e12a5b83141020db9dd5a
AigarsO <aigarsozers@gmail.com>
2018-11-29 13:09:39 +0200

    This is a new commit for what I originally planned to be amended

---
+++ libs/lightbox/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mana pirmā mājaslapa</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type href="reset.css">
+</head>
+<body>
+    <div class="mainbox">
+    <div class="menu">
+<u1>
+    <li><a href="index.php" class="active">Jaunumi</a></li>
+    <li><a href="libs/galerija.html">Galerija</a></li>
+    <li><a href="libs/saziņa.html">Saziņa</a></li>
+    <li><a href="libs/kontakti.html">Kontakti</a></li>
+</u1>
+</div>
+    <div class="leftside">left</div>
+    <div class="main">main</div>
+    <div class="rightside">right</div>
+    <div class="footer">footer</div>
+
+</div>
+</body>
+</html>
